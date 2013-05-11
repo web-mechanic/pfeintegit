5488d5dd2263feb43b3e52d2787a7f8fb1096ccf
Thomas Lissens <th.lissens@gmail.com>
2013-05-11 15:47:53 +0200

    Initial commit

---
+++ maquette/js/main.js
@@ -0,0 +1,19 @@
+  $(function () {
+    $(".mainNav").tinyNav();
+  });
+
+
+$(function() {
+	$(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('.toTop').fadeIn();	
+		} else {
+			$('.toTop').fadeOut();
+		}
+	});
+ 
+	$('.toTop').click(function() {
+		$('body,html').animate({scrollTop:0},800);
+	});	
+});
+
