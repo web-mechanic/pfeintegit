477401882464a64c55fc8cf6fc035dc9831f91da
Thomas Lissens <th.lissens@gmail.com>
2013-05-14 16:38:18 +0200

    Contact form and page

---
+++ maquette/html/contact.php
@@ -0,0 +1,225 @@
+
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+       
+        <title>La royale harmonie concorde Saint-Martin d'Antheit</title>
+        <meta name="description" content="L'harmonie d'antheit joue pour vous un répertoire allant de la musique classique au musique les plus contemporaines.">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <script type="text/javascript" src="//use.typekit.net/lqz6sck.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+        <link rel="stylesheet" href="../css/main.css">
+        
+
+        
+    </head>
+
+    <?php
+
+if(isset($_POST['submit'])) {
+
+    //On s'assure que le champ du nom est bien rempli
+    if(trim($_POST['contactname']) == '') {
+        $hasError = true;
+    } else {
+        $name = trim($_POST['contactname']);
+    }
+
+    //On vérifie que le sujet n'est pas vide
+    if(trim($_POST['subject']) == '') {
+        $hasError = true;
+    } else {
+        $subject = trim($_POST['subject']);
+    }
+
+    //On s'assure que l'e-mail est valide
+    if(trim($_POST['email']) == '')  {
+        $hasError = true;
+    } else if (!preg_match("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$^", trim($_POST['email']))) {
+        $hasError = true;
+    } else {
+        $email = trim($_POST['email']);
+    }
+
+    //On s'assure que tout a été correctement
+    if(trim($_POST['message']) == '') {
+        $hasError = true;
+    } else {
+        if(function_exists('stripslashes')) {
+            $comments = stripslashes(trim($_POST['message']));
+        } else {
+            $comments = trim($_POST['message']);
+        }
+    }
+
+    //Si il n'y a pas d'erreur, on envoit le message
+    if(!isset($hasError)) {
+        $emailTo = 'th.lissens@gmail.com'; //Put your own email address here
+        $body = "Name: $name \n\nEmail: $email \n\nSubject: $subject \n\nComments:\n $comments";
+        $headers = 'From: My Site <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+        mail($emailTo, $subject, $body, $headers);
+        $emailSent = true;
+    }
+}
+?>
+    <body id="accueil">
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+<div class="header">
+<div class="wrap">
+ <h1 class="logoSite"><img class="logo" src="../img/logo.png" alt="Logo de l'harmonie" width="270" height="97"/></h1>
+
+     <nav >
+        <ul class="mainNav">
+            <li><a href="../index.html">Accueil</a></li>
+            <li><a href="apropos.html">À propos</a></li>
+            <li><a href="media.html">Média</a></li>
+            <li><a href="agenda.html">Agenda</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="contact.php">Nous Contacter</a></li>
+        </ul>
+    </nav>
+</div>
+</div>
+<div class="wrap">
+<section class="contact">
+    <h1>Nous contacter</h1>
+    <div class="wrapForm">
+    <div class="iContact">
+        <h2>Via le formulaire de contact</h2>
+        
+        <p>Si vous souhaitez nous contacter directement, nous vous encourageons à employer le formulaire de contact de cette page</p>
+
+        <p>Notre équipe mettra tout en oeuvre afin de vous répondre dans les plus brefs délais.</p>
+
+        <p>Nous avons mis à votre disposition une google map qui vous permettra de trouver notre local de répétition en toute facilité.</p>
+    </div>
+
+
+    <div id="contactWrapper" role="form">
+        <?php if(isset($hasError)) { //If errors are found  ?>  
+            <p class="error">Avez-vous bien remplis toutes les informations?</p>
+        <?php } ?> 
+
+        <?php if(isset($emailSent) && $emailSent == true) { //If email is sent  ?> 
+            <div class="success">
+                <p><strong>Votre e-mail a bien été envoyé!</strong></p>
+                <p>Merci pour nous avoir contactez <strong><?php echo $name;?></strong>!Nous répondrons à votre e-mail dans les plus brefs délais</p>
+            </div>
+        <?php } ?>
+
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactform">
+            <div class="stage clear topForm">
+                <label for="name">Nom: <em>*</em></label>
+                <input type="text" name="contactname" id="contactname" value="" placeholder="Pierre Malempré" class="required" role="input" aria-required="true" />
+            </div>
+
+            <div class="stage clear topForm">
+                <label for="email">Email: <em>*</em></label>
+                <input type="email" name="email" id="email" value="" class="required email" placeholder="p.malempre@yourprovider.com" role="input" aria-required="true" />
+            </div>
+
+            <div class="stage clear">
+                <label for="subject">Sujet: <em>*</em></label>
+                <input type="text" name="subject" id="subject" value="" class="required" role="input" aria-required="true" />
+            </div>
+
+            <div class="stage clear">
+                <label for="message">Message: <em>*</em></label>
+                <textarea rows="8" name="message" id="message" class="required" role="textbox" aria-required="true"></textarea>
+            </div>
+                                
+            <p class="requiredNote"><em>*</em>Indique un champ requis</p>
+            
+            <input type="submit" value="Send Message" name="submit" id="submitButton" title="Click here to submit your message!" />
+        </form>
+        
+    </div>
+    </div>
+    <div class="otherContactInf">
+       <div class="adressAdmin">
+            <h2>Contacter l'administration</h2>
+           <p>  
+                Thomas Lissens<br/>
+                5, rue Basses-Trixhes <br/>
+                4218 Couthuin<br/>
+           </p>
+
+           <p>
+               0472 41 55 89
+           </p>
+
+           <p>
+               admin@harmonie-antheit.be
+           </p>
+
+           <h2>Adresse du local</h2>
+           <p>
+               
+
+           </p>
+       </div>
+    </div>
+
+</section>    
+</div>
+<div class="footer">
+    <div class="wrap">
+    <section class="siteMap">
+      <h1>Site map</h1>
+        <ul>
+            <li><a href="../index.html">Accueil</a></li>
+            <li><a href="apropos.html">À propos</a></li>
+            <li><a href="media.html">Média</a></li>
+            <li><a href="agenda.html">Agenda</a></li>
+            <li><a href="contact.php">Nous Contacter</a></li>
+        </ul>
+    </section>
+
+    <section class="contactFoot">
+        <h1>Nous contacter</h1>
+        <p class="nameAssoc">Harmonie d'Antheit</p>
+        <p class="streetAssoc">40, rue des grandes ruelles</p>
+        <p class="cpVilleAssoc">4520 Wanze</p>
+        <p class="emailAssoc">harmonieantheit@skynet.be</p>
+        <p class="telAssoc">+32 (0)472 41 55 89</p>
+    </section>
+
+    <section class="autresInfos">
+        <h1 class="presseEtRelations">Presse et relations</h1>
+        <ul>
+            <li><a href="html/presse.html">Presse</a></li>
+            <li><a href="html/liensdivers.html">Liens divers</a></li>
+        </ul>
+            </section>
+        <section class="autresInfos">
+        <h1 class="presseEtRelations">Presse et relations</h1>
+        <ul>
+            <li><a href="html/presse.html">Presse</a></li>
+            <li><a href="html/liensdivers.html">Liens divers</a></li>
+        </ul>
+    </section>
+
+    <p class="signature">Copyright "La Royale Harmonie Concorde Saint-Martind'Antheit". Tous droits réservés.</p>
+    </div>
+</div>
+
+
+   
+<script src="//code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="../js/tinynav.min.js"></script>
+<script src="../js/main.js"></script>
+<script src="../js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+     </body>
+
+
+    </html>
--- maquette/js/main.js
+++ maquette/js/main.js
@@ -1,19 +1,72 @@
-  $(function () {
+//scroll vers le haut
+
+$(document).ready(function () {
     $(".mainNav").tinyNav();
-  });
-
-
-$(function() {
-	$(window).scroll(function() {
-		if($(this).scrollTop() != 0) {
-			$('.toTop').fadeIn();	
-		} else {
-			$('.toTop').fadeOut();
-		}
-	});
- 
-	$('.toTop').click(function() {
-		$('body,html').animate({scrollTop:0},800);
-	});	
 });
 
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() !== 0) {
+            $('.toTop').fadeIn();
+        } else {
+            $('.toTop').fadeOut();
+        }
+    });
+
+    $('.toTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+    });
+});
+
+//Validation du formulaire
+
+$(document).ready(function () {
+    // validate signup form on keyup and submit
+    var validator = $("#contactform").validate({
+        rules: {
+            contactname: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            subject: {
+                required: true,
+                minlength: 2
+            },
+            message: {
+                required: true,
+                minlength: 10
+            }
+        },
+        messages: {
+            contactname: {
+                required: "Merci d'entrer votre nom",
+                minlength: jQuery.format("Votre nom doit faire au moins 2 caractères")
+            },
+            email: {
+                required: "Merci d'entrer une adresse e-mail valide",
+                minlength: "Merci d'entrer une adresse e-mail valide"
+            },
+            subject: {
+                required: "Merci de préciser un sujet",
+                minlength: jQuery.format("Merci de préciser un sujet d'au moins 2 caractères")
+            },
+            message: {
+                required: "Et un message! C'est mieux si vous souhaitez vraiment nous contacter :)",
+                minlength: jQuery.format("De plus de {0} caractères")
+            }
+        },
+        // set this class to error-labels to indicate valid fields
+        success: function (label) {
+            label.addClass("checked");
+        }
+
+
+    });
+});
